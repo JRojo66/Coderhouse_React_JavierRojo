c676444ef692953c51e66abd766002bd8678d602
jrojo66 <javier.rojo66@gmail.com>
2024-01-10 20:22:29 -0300

    Movi los assets debajo de la carpeta public

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,11 @@
-import amazonLogo from "../assets/logoRojozon.png"
+import rojozonLogo from "../../public/assets/logoRojozon.png"
 import CartWidget from "./CartWidget";
 
 const NavBar = () => {
     return(
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
-                <img src={amazonLogo} className="navbar-brand logo" alt="amazon" />
+                <img src={rojozonLogo} className="navbar-brand logo" alt="amazon" />
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
