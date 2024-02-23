cc5a05048df2e6542bee36cc4afaa7323a7d4b8b
jrojo66 <javier.rojo66@gmail.com>
2024-02-23 20:09:35 -0300

    Entrega final version 1.5

--- src/components/LogoRojozon.jsx
+++ src/components/LogoRojozon.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import Logo from "../assets/logoRojozon.png";
+// import Logo from "../assets/logoRojozon.png";
 
 const LogoRojozon = ({ tamano }) => {
   return (
