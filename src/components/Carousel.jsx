d39f1390055364e92b62fd6fae426e5d00aad225
jrojo66 <javier.rojo66@gmail.com>
2024-02-23 21:04:44 -0300

    Entrega final version 1.7

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -3,25 +3,25 @@ const Carousel = () => {
     <div id="carouselExample" className="carousel slide">
       <div className="carousel-inner banner-altura-400">
         <div className="carousel-item active">
-          <img src="../src/assets/Carousel1.jpg" className="d-block w-100" alt="Expats"/>
+          <img src="../../src/assets/Carousel1.jpg" className="d-block w-100" alt="Expats"/>
         </div>
         <div className="carousel-item">
-          <img src="../src/assets/Carousel2.jpg" className="d-block w-100" alt="Alexa"/>
+          <img src="../../src/assets/Carousel2.jpg" className="d-block w-100" alt="Alexa"/>
         </div>
         <div className="carousel-item">
-          <img src="../src/assets/Carousel3.jpg" className="d-block w-100" alt="Whole foods"/>
+          <img src="../../src/assets/Carousel3.jpg" className="d-block w-100" alt="Whole foods"/>
         </div>
         <div className="carousel-item">
-          <img src="../src/assets/Carousel4.jpg" className="d-block w-100" alt="Valentine's day"/>
+          <img src="../../src/assets/Carousel4.jpg" className="d-block w-100" alt="Valentine's day"/>
         </div>
         <div className="carousel-item">
-          <img src="../src/assets/Carousel5.jpg" className="d-block w-100" alt="Lego"/>
+          <img src="../../src/assets/Carousel5.jpg" className="d-block w-100" alt="Lego"/>
         </div>
         <div className="carousel-item">
-          <img src="../src/assets/Carousel6.jpg" className="d-block w-100" alt="One medical" />
+          <img src="../../src/assets/Carousel6.jpg" className="d-block w-100" alt="One medical" />
         </div>
         <div className="carousel-item">
-          <img src="../src/assets/Carousel7.jpg" className="d-block w-100" alt="Winter sale"/>
+          <img src="../../src/assets/Carousel7.jpg" className="d-block w-100" alt="Winter sale"/>
         </div>
       </div>
       <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
