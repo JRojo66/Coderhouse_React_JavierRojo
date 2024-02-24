9061a42cd9823605a1149931223b964c2401e594
jrojo66 <javier.rojo66@gmail.com>
2024-02-23 21:19:47 -0300

    Entrega final version 1.8

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -3,22 +3,22 @@ const Carousel = () => {
     <div id="carouselExample" className="carousel slide">
       <div className="carousel-inner banner-altura-400">
         <div className="carousel-item active">
-          <img src="../../src/assets/Carousel1.jpg" className="d-block w-100" alt="Expats"/>
+          <img src="https://i.ibb.co/3C1hPK3/Carousel1.jpg" className="d-block w-100" alt="Expats"/>
         </div>
         <div className="carousel-item">
-          <img src="../../src/assets/Carousel2.jpg" className="d-block w-100" alt="Alexa"/>
+          <img src="https://i.ibb.co/sWkHJPr/Carousel2.jpg" className="d-block w-100" alt="Alexa"/>
         </div>
         <div className="carousel-item">
-          <img src="../../src/assets/Carousel3.jpg" className="d-block w-100" alt="Whole foods"/>
+          <img src="https://i.ibb.co/j8bNnMd/Carousel3.jpg" className="d-block w-100" alt="Whole foods"/>
         </div>
         <div className="carousel-item">
-          <img src="../../src/assets/Carousel4.jpg" className="d-block w-100" alt="Valentine's day"/>
+          <img src="https://i.ibb.co/wgbm8Ls/Carousel4.jpg" className="d-block w-100" alt="Valentine's day"/>
         </div>
         <div className="carousel-item">
-          <img src="../../src/assets/Carousel5.jpg" className="d-block w-100" alt="Lego"/>
+          <img src="https://i.ibb.co/m02SyCZ/Carousel5.jpg" className="d-block w-100" alt="Lego"/>
         </div>
         <div className="carousel-item">
-          <img src="../../src/assets/Carousel6.jpg" className="d-block w-100" alt="One medical" />
+          <img src="https://i.ibb.co/hYWsTgj/Carousel6.jpg" className="d-block w-100" alt="One medical" />
         </div>
         <div className="carousel-item">
           <img src="../../src/assets/Carousel7.jpg" className="d-block w-100" alt="Winter sale"/>
